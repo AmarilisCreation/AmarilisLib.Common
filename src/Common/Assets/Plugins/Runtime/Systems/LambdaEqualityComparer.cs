b8171cf6863a4d330aabf0969d42d3f0e6d15fcc
AmarilisCreation <tfb00plzdj+AmarilisCreation@gmail.com>
2024-08-24 15:05:48 +0900

    Add LambdaEqualityComparer

---
+++ src/Common/Assets/Plugins/Runtime/Systems/LambdaEqualityComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AmarilisLib
+{
+    /// <summary>
+    /// Provides an equality comparer for objects of type <typeparamref name="T"/> 
+    /// based on a key extracted from the object using a lambda expression.
+    /// </summary>
+    /// <typeparam name="T">The type of objects to compare.</typeparam>
+    /// <typeparam name="TKey">The type of the key used for comparison.</typeparam>
+    public class LambdaEqualityComparer<T, TKey> : IEqualityComparer<T>
+    {
+        private readonly Func<T, TKey> _selector;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LambdaEqualityComparer{T, TKey}"/> class.
+        /// </summary>
+        /// <param name="selector">A function to extract the key from an object of type <typeparamref name="T"/>.</param>
+        public LambdaEqualityComparer(Func<T, TKey> selector)
+        {
+            _selector = selector ?? throw new ArgumentNullException(nameof(selector));
+        }
+
+        bool IEqualityComparer<T>.Equals(T x, T y)
+            => _selector(x).Equals(_selector(y));
+
+        int IEqualityComparer<T>.GetHashCode(T obj)
+            => _selector(obj).GetHashCode();
+    }
+}
